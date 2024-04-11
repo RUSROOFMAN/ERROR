c6e3edad349faa8dc11c08edc55c025cde1ce06c
Sergey Bondarev <bondarevsergeq1@mail.ru>
2024-04-11 22:41:40 +0700

    Change code

---
+++ src/test/java/ShopRepositoryTest.java
@@ -0,0 +1,59 @@
+import ProductManager.NotFoundException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ShopRepositoryTest {
+    @Test
+    public void testRemoveProductNotExist() {
+        ShopRepository repository = new ShopRepository();
+        Product product1 = new Product(1, "телефон", 30_000);
+        Product product2 = new Product(2, "книга", 300);
+        Product product3 = new Product(3, "машина", 3_000_000);
+
+        repository.add(product1);
+        repository.add(product2);
+        repository.add(product3);
+        repository.remove(234526);
+
+        Product[] actual = repository.findAll();
+        Product[] expected = {product1, product2, product3};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void testRemoveProductWhenExist() {
+        ShopRepository repository = new ShopRepository();
+        Product product1 = new Product(1, "телефон", 30_000);
+        Product product2 = new Product(2, "книга", 300);
+        Product product3 = new Product(3, "машина", 3_000_000);
+
+        repository.add(product1);
+        repository.add(product2);
+        repository.add(product3);
+        repository.remove(234526);
+
+        Product[] actual = repository.findAll();
+        Product[] expected = {product1,product3};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemoveWhenProductNotExist() {
+        ShopRepository repository = new ShopRepository();
+        Product product1 = new Product(1, "телефон", 30_000);
+        Product product2 = new Product(2, "книга", 300);
+        Product product3 = new Product(3, "машина", 3_000_000);
+
+        repository.add(product1);
+        repository.add(product2);
+        repository.add(product3);
+        repository.remove(234526);
+
+        Product[] actual = repository.findAll();
+        Product[] expected = {product1, product2, product3};
+        Assertions.assertThrows(NotFoundException.class,
+                () -> repository.remove(123123)
+        );
+
+
+    }
+}
