65c374a7dd1edd4e5ad1c8539b91185075b55dec
Sergey Bondarev <bondarevsergeq1@mail.ru>
2024-04-11 23:13:30 +0700

    Repair code

--- src/test/java/ShopRepositoryTest.java
+++ src/test/java/ShopRepositoryTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 public class ShopRepositoryTest {
     @Test
-    public void testRemoveProductNotExist() {
+    public void testRemoveWhenProductExist() {
         ShopRepository repository = new ShopRepository();
         Product product1 = new Product(1, "телефон", 30_000);
         Product product2 = new Product(2, "книга", 300);
@@ -13,26 +13,10 @@ public class ShopRepositoryTest {
         repository.add(product1);
         repository.add(product2);
         repository.add(product3);
-        repository.remove(234526);
+        repository.remove(2);
 
         Product[] actual = repository.findAll();
-        Product[] expected = {product1, product2, product3};
-        Assertions.assertArrayEquals(expected, actual);
-    }
-    @Test
-    public void testRemoveProductWhenExist() {
-        ShopRepository repository = new ShopRepository();
-        Product product1 = new Product(1, "телефон", 30_000);
-        Product product2 = new Product(2, "книга", 300);
-        Product product3 = new Product(3, "машина", 3_000_000);
-
-        repository.add(product1);
-        repository.add(product2);
-        repository.add(product3);
-        repository.remove(234526);
-
-        Product[] actual = repository.findAll();
-        Product[] expected = {product1,product3};
+        Product[] expected = {product1, product3};
         Assertions.assertArrayEquals(expected, actual);
     }
 
@@ -46,13 +30,9 @@ public class ShopRepositoryTest {
         repository.add(product1);
         repository.add(product2);
         repository.add(product3);
-        repository.remove(234526);
 
-        Product[] actual = repository.findAll();
-        Product[] expected = {product1, product2, product3};
         Assertions.assertThrows(NotFoundException.class,
                 () -> repository.remove(123123)
         );
-        
     }
 }
